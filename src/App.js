b91c2974fac6c83eedf860eb0aa1b4b664bcaddf
gwenael2bihan <33218283+gwenael2bihan@users.noreply.github.com>
2021-01-14 01:03:21 +0100

    Portfolio propre

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+import Banner from "./components/Banner"
+import Nav from "./components/Nav"
+import Skills from './components/Skills';
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Nav />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
